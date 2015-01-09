c0aafc7036d94290d79841ab686db7b9f636cf04
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-09 07:11:23 +0100

    fix autoloader to include path (include_path might be unset etc.)

--- autoload.php
+++ autoload.php
@@ -2,6 +2,7 @@
 
 spl_autoload_register(function($class) {
     $file = str_replace(['\\', '_'], DIRECTORY_SEPARATOR, $class).'.php';
-    include $file;
+    $file = preg_replace('@^disclosure@', '', $file);
+    include realpath(__DIR__).$file;
 });
 
