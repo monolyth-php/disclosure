31345e159a8c4f4dd9622a27aac7412b6d76d2eb
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-09 07:11:35 +0100

    implement inject method

--- Injector.php
+++ Injector.php
@@ -6,6 +6,31 @@ trait Injector
 {
     private function inject()
     {
+        $args = func_get_args();
+        if (is_callable(end($args))) {
+            $callback = array_pop($args);
+        }
+        $deps = [];
+        $missing = false;
+        foreach ($args as $arg) {
+            try {
+                $deps[$arg] = call_user_func(Container::get($arg));
+            } catch (UnregisteredException $e) {
+                $missing = true;
+                $deps[$arg] = null;
+            }
+        }
+        if (isset($callback)) {
+            $deps = call_user_func_array($callback, $deps);
+        }
+        foreach ($deps as $name => $value) {
+            if (isset($value)) {
+                $this->$name = $value;
+            }
+        }
+        if ($missing) {
+            throw new UnregisteredException;
+        }
     }
 
     private function injectAs()
