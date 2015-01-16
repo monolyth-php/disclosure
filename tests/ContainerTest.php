73c29b27f5a42b4917c9750f08b8ac6d065c8fcf
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-16 13:20:57 +0100

    test unregistered

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -2,6 +2,7 @@
 
 use Disclosure\Container;
 use Disclosure\Injector;
+use Disclosure\UnregisteredException;
 
 $container = new Container;
 $container->register('Bar');
@@ -18,6 +19,17 @@ class Foo
     {
         $this->disclosureContainer = $container;
         $this->inject('Bar', 'Alias', 'Service', 'Service2', 'Service3');
+    }
+}
+
+class FooUnregistered
+{
+    use Injector;
+
+    public function __construct(Container $container)
+    {
+        $this->disclosureContainer = $container;
+        $this->inject('SomethingUnregistered');
     }
 }
 
@@ -51,6 +63,17 @@ class ContainerTest extends PHPUnit_Framework_TestCase
         $foo = new Foo($container);
         $this->assertSame($foo->Service, $foo->Service2);
         $this->assertNotSame($foo->Service, $foo->Service3);
+    }
+
+    public function testUnregistered()
+    {
+        global $container;
+        $e = null;
+        try {
+            $foo = new FooUnregistered($container);
+        } catch (Exception $e) {
+        }
+        $this->assertInstanceOf('Disclosure\UnregisteredException', $e);
     }
 }
 
