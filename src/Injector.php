36ed844499eea0bb2c74d47e12bf2d03fe1beb93
Marijn Ophorst <marijn@sensimedia.nl>
2015-01-19 11:06:42 +0100

    hm

--- src/Injector.php
+++ src/Injector.php
@@ -3,6 +3,67 @@
 namespace Disclosure;
 use ReflectionFunction;
 
+function _($class, array $injects)
+{
+    static $map = [];
+
+    $resolve = function ($for, $class) use ($map) {
+    };
+
+    $tree = [];
+    do {
+        $tree[] = $class;
+    } while ($class = get_parent_class($class));
+    $class = $tree[0];
+
+    if (!isset($map[$class])) {
+        $map[$class] = [];
+    }
+    var_dump($class, $map[$class]);
+    foreach ($injects as $name => &$cname) {
+        if (!is_scalar($cname)) {
+            $map[$class][$name] = $cname;
+            continue;
+        }
+        foreach ($map[$class] as $key => $resolved) {
+            if ($resolved instanceof $cname) {
+                $cname = $resolved;
+                continue 2;
+            }
+        }
+        if (!is_bool($cname)) {
+            if (!isset($map[$class][$name])) {
+                $map[$class][$name] = $cname;
+            } elseif (!is_string($map[$class][$name])
+                && get_class($map[$class][$name]) == $cname
+            ) {
+                $cname = $map[$class][$name];
+            }
+        }
+        /*
+        foreach ($tree as $parent) {
+            if (isset($map[$parent])) {
+                foreach ($map[$parent] as $previous) {
+                    if (!is_string($previous)
+                        && !is_object($cname)
+                        && get_class($previous) == $cname
+                    ) {
+                        $cname = $previous;
+                        break 2;
+                    }
+                }
+            }
+        }
+        */
+        if (is_string($cname) && class_exists($cname)) {
+            $cname = new $cname;
+            $map[$class][$name] = $cname;
+        }
+    }
+    var_dump($class, $map[$class]);
+    return $injects;
+}
+
 trait Injector
 {
     public function inject(callable $inject)
@@ -11,55 +72,36 @@ trait Injector
         $class = $static ? __CLASS__ : get_class($this);
         $reflection = new ReflectionFunction($inject);
         $parameters = $reflection->getParameters();
-//        $injectable = $inject();
-        // For single arguments, allow shorthand of single value return.
-        /*
-        if (!is_array($injectable)) {
-            $injectable = [$injectable];
-        }
-        if ($static) {
-            $inject = [];
-            foreach ($parameters as $idx => $value) {
-                var_dump($value);
-                $inject[$value->name] = $injectable[$idx];
+        $classes = [];
+        foreach ($parameters as $idx => $parameter) {
+            if ($requestedclass = $parameter->getClass()) {
+                $classes[$parameter->name] = $requestedclass->name;
+            } else {
+                $classes[$parameter->name] = true;
             }
-            var_dump(get_parent_class($class));
-            Container::register($class, $inject);
         }
-        var_dump($parameters);
-        if ($static) {
-            Container::
-        var_dump($static, $class);
-        /*
-        $args = func_get_args();
-        if (is_callable(end($args))) {
-            $callback = array_pop($args);
+        $injections = _($class, $classes);
+        $previous = $injections;
+        var_dump(1, $injections);
+        $reflection->invokeArgs($injections);
+        var_dump(2, $injections);
+        if ($previous != $injections) {
+            _($class, $injections);
         }
-        $deps = [];
+        var_dump(3, $injections);
         $missing = false;
-        $container = Container::instance();
-        foreach ($args as $arg) {
-            try {
-                $deps[$arg] = call_user_func(
-                    $container->get($arg)
-                );
-            } catch (UnregisteredException $e) {
-                $missing = true;
-                $deps[$arg] = null;
-            }
-        }
-        if (isset($callback)) {
-            $deps = call_user_func_array($callback, $deps);
-        }
-        foreach ($deps as $name => $value) {
-            if (isset($value)) {
-                $this->$name = $value;
+        if (!$static) {
+            foreach ($injections as $requested => $value) {
+                if (is_string($value)) {
+                    $missing = true;
+                    continue;
+                }
+                $this->$requested = $value;
             }
         }
         if ($missing) {
-            throw new UnregisteredException;
+            throw new UninjectableException;
         }
-        */
     }
 }
 
